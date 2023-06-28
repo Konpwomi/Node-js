6355ef9ba66f4e0f97272f533975e27232d4351a
Patiphan Arphorn <Patiphan.arp@gmail.com>
2023-06-28 11:52:07 +0700

    Add all file of project
    First test

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const chalk = require('chalk')
+const debug = require('debug')('app');
+const morgan = require('morgan');
+
+const app = express();
+const port = 3000;
+
+app.use(morgan('combined'));
+
+
+app.get("/", (req,res) =>{
+
+    res.send('Hello Patiphan Arphorn');
+
+})
+
+app.listen(port, ()=>{
+
+    debug("Listening on port : " + chalk.green(port));
+
+})
